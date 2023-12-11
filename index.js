3bd8721319ef273a5a5ac46c11ff4ee91e8f4f10
Maxim <medey4work@gmail.com>
2023-12-11 13:59:24 +0300

    начал движение tileE

--- index.js
+++ index.js
@@ -1,159 +1,299 @@
 'use strict';
 
 const field = document.querySelector('.field');
-const player = {
-   maxHealth: 10,
-   health: 1,
-   attackPower: 1,
-}
+
+// const player = {
+//    tile: 'tileP',
+//    maxHealth: 10,
+//    health: 5,
+//    attackPower: 5,
+// };
+
+class Player {
+   name = 'p-1';
+
+   state = {
+      tile: 'tileP',
+      maxHealth: 10,
+      health: 5,
+      attackPower: 1,
+   }
+};
+
+const player = new Player();
+
+class Enemy {
+   constructor(name) {
+      this.name = name;
+   };
+
+   state = {
+      tile: 'tileE',
+      maxHealth: 3,
+      health: 3,
+      attackPower: 1
+   };
+
+   enemyTile() {
+      return document.getElementById(this.name);
+   };
+
+   currentTile() {
+      return arrayField.indexOf(document.getElementById(this.name));
+   };
+
+};
 
 // ===================
-// Параметры для класса
 const maxWidth = 40,
    maxHeight = 24;
 // ===================
 
-for (let i = 1; i < maxWidth * maxHeight; i++) {
+for (let i = 0; i < maxWidth * maxHeight; i++) {
    const tile = document.createElement('div');
-   tile.className = 'tile';
-   field.append(tile);
+
+   if (i == 0) {
+      tile.className = 'tileP';
+      field.append(tile);
+   } else if (i == 47 || i == 15 || i == 14 || i == 40) {
+      tile.className = 'tileE';
+      tile.id = `e-${i}`;
+      field.append(tile);
+   } else if (i == 2) {
+      tile.className = 'tileHP';
+      field.append(tile);
+   } else if (i == 1) {
+      tile.className = 'tileSW';
+      field.append(tile);
+   }
+   else {
+      tile.className = 'tile';
+      field.append(tile);
+   }
 }
 
 
-let arrField = [...field.children]; // == Array.from()
+const arrayField = [...field.children]; // == Array.from()
+const collectionEnemy = document.querySelectorAll('.tileE');
+// console.log(arrayField);
 
-console.log(arrField);
+// ===================
+// arrey enemy
+let arrayEnemy = [];
+collectionEnemy.forEach(item => {
+   arrayEnemy.push(new Enemy(item.id));
+
+});
+// console.log(arrayEnemy); 
+// console.log(arrayEnemy[0].name);
+// ===================
+
+function randomSide(min, max) {
+   const rand = min + Math.random() * (max + 1 - min);
+   return Math.floor(rand);
+}
+
+function onMove(char) {
+   const currentTile = char.currentTile();
+
+   let side = randomSide(1, 4);
+
+   // направо
+   if (side == 1 && (currentTile == 0 || (currentTile + 1) % maxWidth != 0)) {
+      const nextTile = arrayField[currentTile + 1];
+
+      // setPlayerState(nextTile.className);
+      setTileState(arrayField[currentTile], nextTile, char);
+   }
+   // налево
+   if (side == 2 && (currentTile % maxWidth != 0)) {
+      const nextTile = arrayField[currentTile - 1];
+
+      // setPlayerState(nextTile.className);
+      setTileState(arrayField[currentTile], nextTile, char);
+   }
+   // вниз
+   if (side == 3 && (currentTile + maxWidth < arrayField.length)) {
+      const nextTile = arrayField[currentTile + maxWidth];
+
+      // setPlayerState(nextTile.className);
+      setTileState(arrayField[currentTile], nextTile, char);
+   }
+   // вверх
+   if (side == 4 && (currentTile - maxWidth >= 0)) {
+      const nextTile = arrayField[currentTile - maxWidth];
+
+      // setPlayerState(nextTile.className);
+      setTileState(arrayField[currentTile], nextTile, char);
+   }
+}
+
 
 document.addEventListener('keydown', event => {
    const playerTile = document.querySelector('.tileP');
-   const currentTile = arrField.indexOf(playerTile);
+   const currentTile = arrayField.indexOf(playerTile);
 
    // направо
    if (event.code == 'KeyD' && (currentTile == 0 || (currentTile + 1) % maxWidth != 0)) {
-      const nextTile = arrField[currentTile + 1];
+      const nextTile = arrayField[currentTile + 1];
 
       setPlayerState(nextTile.className);
-      setTileState(arrField[currentTile], nextTile);
-   }
+      setTileState(arrayField[currentTile], nextTile, player);
 
+      enemyAction();
+
+   }
    // налево
    if (event.code == 'KeyA' && (currentTile % maxWidth != 0)) {
-      const nextTile = arrField[currentTile - 1];
+      const nextTile = arrayField[currentTile - 1];
 
       setPlayerState(nextTile.className);
-      setTileState(arrField[currentTile], nextTile);
-   }
+      setTileState(arrayField[currentTile], nextTile, player);
 
+      enemyAction();
+
+   }
    // вниз
-   if (event.code == 'KeyS' && (currentTile + maxWidth < arrField.length)) {
-      const nextTile = arrField[currentTile + maxWidth];
+   if (event.code == 'KeyS' && (currentTile + maxWidth < arrayField.length)) {
+      const nextTile = arrayField[currentTile + maxWidth];
 
       setPlayerState(nextTile.className);
-      setTileState(arrField[currentTile], nextTile);
-   }
+      setTileState(arrayField[currentTile], nextTile, player);
 
+      enemyAction();
+
+   }
    // вверх
    if (event.code == 'KeyW' && (currentTile - maxWidth >= 0)) {
-      const nextTile = arrField[currentTile - maxWidth];
+      const nextTile = arrayField[currentTile - maxWidth];
 
       setPlayerState(nextTile.className);
-      setTileState(arrField[currentTile], nextTile);
+      setTileState(arrayField[currentTile], nextTile, player);
+
+      enemyAction();
+
    }
 
    if (event.code == 'Space') {
       event.preventDefault();
       onAttack(currentTile);
+
+
+
+      // enemyAction();
    }
 });
 
 function setPlayerState(classTile) {
    switch (classTile) {
       case 'tileHP':
-         if (player.health < player.maxHealth) {  // * фиксированный порог HP
-            player.health = player.health + 1;
+         if (player.state.health < player.state.maxHealth) {  // * фиксированное
+            player.state.health = player.state.health + 1;
          }
          console.log(player);
          break;
 
       case 'tileSW':
-         player.attackPower = player.attackPower + 1;
+         player.state.attackPower = player.state.attackPower + 1;
          console.log(player);
          break;
    }
 }
 
-function setTileState(currentTile, nextTile) {
-   currentTile.innerHTML = '';
-   currentTile.className = 'tile';
-   nextTile.className = 'tileP';
-
+function setTileState(currentTile, nextTile, char) {
    const health = document.createElement('div');
    health.className = 'health';
-   health.style.width = `${player.health * 100 / player.maxHealth}%`;
-   nextTile.append(health);
+   health.style.width = `${char.state.health * 100 / char.state.maxHealth}%`;
+
+
+   // if (currentTile == nextTile) {
+   if (arrayField.indexOf(currentTile) == arrayField.indexOf(nextTile)) {
+      // console.log(arrayField.indexOf(arrayField[currentTile]), arrayField.indexOf(nextTile));
+
+      console.log('== stay');
+
+
+      currentTile.innerHTML = '';
+      currentTile.append(health);
+   }
+   if ((arrayField.indexOf(currentTile) != arrayField.indexOf(nextTile))
+      && !(nextTile.classList.contains('tileE') || nextTile.classList.contains('tileW') || nextTile.classList.contains('tileP'))) {
+      currentTile.innerHTML = '';
+      currentTile.className = 'tile';
+      currentTile.id = '';
+      nextTile.className = char.state.tile;
+      nextTile.id = char.name;
+      nextTile.append(health);
+   }
+}
+
+function enemyAction() {
+   // set enemy HP
+   arrayEnemy.forEach(char => {
+      // setTileState(char.enemyTile(), char.enemyTile(), char);
+
+      onMove(char);
+   });
+
+
+
 }
 
 function onAttack(currentTile) {
-   const attackArea = [
-      // arrField[currentTile - maxWidth - 1],
-      // arrField[currentTile - maxWidth],
-      // arrField[currentTile - maxWidth + 1],
-      // arrField[currentTile - 1],
-      // arrField[currentTile + 1],
-      // arrField[currentTile + maxWidth - 1],
-      // arrField[currentTile + maxWidth],
-      // arrField[currentTile + maxWidth + 1]
-
-      // currentTile - maxWidth - 1,
-      // currentTile - maxWidth,
-      // currentTile - maxWidth + 1,
-      // currentTile - 1,
-      // currentTile + 1,
-      // currentTile + maxWidth - 1,
-      // currentTile + maxWidth,
-      // currentTile + maxWidth + 1
-   ];
-
+   const attackArea = [];
    // правый тайл
-   if (arrField[currentTile + 1] !== 'undefined' && (currentTile + 1) % maxWidth != 0) {
-      attackArea.push(arrField[currentTile + 1]);
+   if (arrayField[currentTile + 1] !== 'undefined' && (currentTile + 1) % maxWidth != 0) {
+      attackArea.push(arrayField[currentTile + 1]);
    }
    // левый тайл
-   if (arrField[currentTile - 1] !== 'undefined' && (currentTile % maxWidth != 0)) {
-      attackArea.push(arrField[currentTile - 1]);
+   if (arrayField[currentTile - 1] !== 'undefined' && (currentTile % maxWidth != 0)) {
+      attackArea.push(arrayField[currentTile - 1]);
    }
    // нижний тайл
-   if (arrField[currentTile + maxWidth] !== 'undefined' && (currentTile + maxWidth < arrField.length)) {
-      attackArea.push(arrField[currentTile + maxWidth]);
+   if (arrayField[currentTile + maxWidth] !== 'undefined' && (currentTile + maxWidth < arrayField.length)) {
+      attackArea.push(arrayField[currentTile + maxWidth]);
    }
    // верхний тайл
-   if (arrField[currentTile - maxWidth] !== 'undefined' && (currentTile - maxWidth >= 0)) {
-      attackArea.push(arrField[currentTile - maxWidth]);
+   if (arrayField[currentTile - maxWidth] !== 'undefined' && (currentTile - maxWidth >= 0)) {
+      attackArea.push(arrayField[currentTile - maxWidth]);
    }
    // нижний правый тайл
-   if (arrField[currentTile + maxWidth] !== 'undefined' && (currentTile + maxWidth < arrField.length) && (currentTile + 1) % maxWidth != 0) {
-      attackArea.push(arrField[currentTile + maxWidth + 1]);
+   if (arrayField[currentTile + maxWidth] !== 'undefined' && (currentTile + maxWidth < arrayField.length) && (currentTile + 1) % maxWidth != 0) {
+      attackArea.push(arrayField[currentTile + maxWidth + 1]);
    }
    // нижний левый тайл
-   if (arrField[currentTile + maxWidth] !== 'undefined' && (currentTile + maxWidth < arrField.length) && currentTile % maxWidth != 0) {
-      attackArea.push(arrField[currentTile + maxWidth - 1]);
+   if (arrayField[currentTile + maxWidth] !== 'undefined' && (currentTile + maxWidth < arrayField.length) && currentTile % maxWidth != 0) {
+      attackArea.push(arrayField[currentTile + maxWidth - 1]);
    }
    // верхний правый тайл
-   if (arrField[currentTile - maxWidth] !== 'undefined' && (currentTile - maxWidth >= 0) && (currentTile + 1) % maxWidth != 0) {
-      attackArea.push(arrField[currentTile - maxWidth + 1]);
+   if (arrayField[currentTile - maxWidth] !== 'undefined' && (currentTile - maxWidth >= 0) && (currentTile + 1) % maxWidth != 0) {
+      attackArea.push(arrayField[currentTile - maxWidth + 1]);
    }
    // верхний левый тайл
-   if (arrField[currentTile - maxWidth] !== 'undefined' && (currentTile - maxWidth >= 0) && currentTile % maxWidth != 0) {
-      attackArea.push(arrField[currentTile - maxWidth - 1]);
+   if (arrayField[currentTile - maxWidth] !== 'undefined' && (currentTile - maxWidth >= 0) && currentTile % maxWidth != 0) {
+      attackArea.push(arrayField[currentTile - maxWidth - 1]);
    }
-
 
 
    // console.log(attackArea);
 
    attackArea.forEach(item => {
       if (item.classList.contains('tileE')) {
-         console.log(item);
+         // console.log(item);
+
+         for (let i = 0; i < arrayEnemy.length; i++) {
+            if (item.id == arrayEnemy[i].name) {
+               arrayEnemy[i].state.health = arrayEnemy[i].state.health - 1;
+
+               console.log(arrayEnemy[i].enemyTile());
+               console.log(arrayEnemy[i].currentTile());
+
+               if (arrayEnemy[i].state.health == 0) {
+                  item.className = 'tile';
+               }
+            }
+         }
+
       }
    });
 }
